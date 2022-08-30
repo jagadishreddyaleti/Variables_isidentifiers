332f0b610992669d7cc4d977215a3ae71447b087
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-08-30 12:06:33 +0530

    Add files via upload

---
+++ Variable or Identifier.py
@@ -0,0 +1,47 @@
+"  30th aug 2022  "
+'''  variables & identifiers'''
+from re import A
+
+
+# print('a b'.isidentifier())
+# print('a,b'.isidentifier())
+# print('a_b'.isidentifier())
+# print('x_y_c'.isidentifier())
+
+# ''' ways to assign variiables '''
+# # a=1
+# # b=2
+# # print(a)
+# # print(b)
+
+
+# # a=1
+# # b=2
+# # print(a,b)
+# # print(a);print(b)
+
+
+# # a=1
+# # # b=2
+# # print(a,a)
+# # print(a);print(b)
+
+# # a=1 ; b=3
+# # print(a,b)
+# # print(a);print(b)
+
+# x,y,z=4,5,6
+# # print(x,y,z)
+# print(z,y,z)
+# print(x+y)
+
+
+# a=b=c=5
+# print(a,b,c)
+
+# a=d=4;b=6
+# print(b,a)
+
+# ahagsdhdjahC=6
+# print(ahagsdhdjahC)
+# print('dgshfghsgfhsgfhsg'.isidentifier())
